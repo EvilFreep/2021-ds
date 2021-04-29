a42fee4ce151d29cb079729b96385dbe48f65c01
EvilFreep <56558082+EvilFreep@users.noreply.github.com>
2021-04-29 19:13:02 +0300

    valuator logic added

---
+++ Valuator/Contains.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Valuator
+{
+    public static class Contains
+    {
+        public const string SimilarityKeyPrefix = "SIMILARITY-";
+        public const string RankKeyPrefix = "RANK-";
+        public const string TextKeyPrefix = "TEXT-";
+    }
+}
